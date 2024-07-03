982bf18d76bdd782dff9db6f6d718f1d90aa2360
piyush dravyakar <piyushdravyakar69@gmail.com>
2024-07-03 16:17:28 +0530

    logout

--- Test_Cases/test_registration_page.py
+++ Test_Cases/test_registration_page.py
@@ -4,27 +4,31 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from PageObjects.registration import Register
+from Test_Cases.test_loginPage import Test_Login
 
 
 class Test_registration:
     """
         Test class for the registration process.
         """
-    EMAIL = "piyush580@gmail.com"
-    MOBILE_NO = "8411878794"
-    PASSWORD = "piyush@123"
+    enter_new_email = "piyush88@gmail.com"
+    enter_mobile_no = "8411878794"
+    enter_password = "piyush@123"
 
 
-def test_register(self,setup):
-        """
-        Test case for user registration.
-        """
-        self.driver = setup
-        self.register = Register(self.driver)
-        self.register.click_login()
-        self.register.send_email(self.enter_new_email)
-        self.register.enter_mobile_no(self.enter_mobile_no)
-        self.register.enter_password(self.enter_password)
-        self.register.click_sign_up()
+    def test_register(self,setup):
+            """
+            Test case for user registration.
+            """
+            self.driver = setup
+            self.driver.implicitly_wait(20)
+            self.register = Register(self.driver)
+            self.register.click_login()
+            self.register.send_email(self.enter_new_email)
+            self.register.enter_mobile_no(self.enter_mobile_no)
+            self.register.enter_password(self.enter_password)
+            self.register.click_sign_up()
+            self.check_name  = self.driver.find_element(By.CSS_SELECTOR, Test_Login().text_match)
+            assert self.check_name.text == "Hi! Reader", "Something went wrong"
 
 
